650151958b63f8c7442560fd8093653a67c73993
stanrails <stansmith@pushingsocial.com>
2015-04-04 23:06:10 -0400

    Debugging Heroku

--- coffeed/wsgi.py
+++ coffeed/wsgi.py
@@ -1,19 +1,5 @@
-"""
-WSGI config for coffeed project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
-"""
-
-
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeed.settings")
-
-#from django.core.wsgi import get_wsgi_application
-#application = get_wsgi_application()
-
-
+from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 application = Cling(get_wsgi_application())
