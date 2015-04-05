abec6a0af567df17daa8175137babe4608dd4d23
stanrails <stansmith@pushingsocial.com>
2015-04-04 21:43:18 -0400

    Rollback to operational version

--- coffeed/wsgi.py
+++ coffeed/wsgi.py
@@ -4,16 +4,11 @@ WSGI config for coffeed project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeed.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
